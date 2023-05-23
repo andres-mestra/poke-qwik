1f3ab84b68c61a3e082330919bd71bf8b637a346
andres-mestra <andresmestra11a@gmail.com>
2023-05-22 22:25:08 -0500

    feat: set infinite scroll on get pokemons

--- src/routes/pokemons/list-client/index.tsx
+++ src/routes/pokemons/list-client/index.tsx
@@ -1,4 +1,10 @@
-import { component$, useStore, useVisibleTask$ } from '@builder.io/qwik'
+import {
+  $,
+  component$,
+  useStore,
+  useTask$,
+  useOnDocument,
+} from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
 import { PokemonImage } from '~/components/pokemons/pokemon-image'
 import { getPokemonsSmall } from '~/helpers/get-pokemos-small'
@@ -8,25 +14,64 @@ import type { PokemonSmall } from '~/interfaces'
 import styles from '../styles.module.css'
 
 interface PokemonPageState {
+  loading: boolean
+  isLast: boolean
   currentPage: number
   pokemons: PokemonSmall[]
 }
 
+const LIMIT = 30
+const SCROLL_SPACE_ON = 200
 export default component$(() => {
   const pokemonState = useStore<PokemonPageState>({
+    loading: false,
+    isLast: false,
     currentPage: 0,
     pokemons: [],
   })
 
+  /*
   //Solo el client
-  useVisibleTask$(async ({ track }) => {
+  // useVisibleTask$(async ({ track }) => {
+  //   track(() => pokemonState.currentPage)
+
+  //   const pokemons = await getPokemonsSmall(
+  //     pokemonState.currentPage * OFFSET_PAD
+  //   )
+  //   pokemonState.pokemons = [...pokemonState.pokemons, ...pokemons]
+  // })
+  */
+
+  //Primero en el server, luego del lado del client si cambia currentPage
+  useTask$(async ({ track }) => {
     track(() => pokemonState.currentPage)
 
     const pokemons = await getPokemonsSmall(
-      pokemonState.currentPage * OFFSET_PAD
+      pokemonState.currentPage * OFFSET_PAD,
+      LIMIT
     )
+
     pokemonState.pokemons = [...pokemonState.pokemons, ...pokemons]
+    pokemonState.loading = false
+    pokemonState.isLast = !pokemons.length
   })
+
+  useOnDocument(
+    'scroll',
+    $(() => {
+      const maxScroll = document.body.scrollHeight
+      const currentScroll = window.scrollY + window.innerHeight
+
+      if (
+        currentScroll + SCROLL_SPACE_ON >= maxScroll &&
+        !pokemonState.loading &&
+        !pokemonState.isLast
+      ) {
+        pokemonState.loading = true
+        pokemonState.currentPage++
+      }
+    })
+  )
 
   return (
     <article class={styles.pokemons__page}>
