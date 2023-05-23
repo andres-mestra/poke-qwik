5868ba41f9611cd11f75d5c357267ecf2b6801e2
andres-mestra <andresmestra11a@gmail.com>
2023-05-22 21:39:22 -0500

    feat: get pokemons from client

--- src/routes/pokemons/list-client/index.tsx
+++ src/routes/pokemons/list-client/index.tsx
@@ -1,6 +1,11 @@
-import { component$, useStore } from '@builder.io/qwik'
+import { component$, useStore, useVisibleTask$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
+import { PokemonImage } from '~/components/pokemons/pokemon-image'
+import { getPokemonsSmall } from '~/helpers/get-pokemos-small'
+import { OFFSET_PAD } from '~/constants'
 import type { PokemonSmall } from '~/interfaces'
+
+import styles from '../styles.module.css'
 
 interface PokemonPageState {
   currentPage: number
@@ -13,8 +18,18 @@ export default component$(() => {
     pokemons: [],
   })
 
+  //Solo el client
+  useVisibleTask$(async ({ track }) => {
+    track(() => pokemonState.currentPage)
+
+    const pokemons = await getPokemonsSmall(
+      pokemonState.currentPage * OFFSET_PAD
+    )
+    pokemonState.pokemons = [...pokemonState.pokemons, ...pokemons]
+  })
+
   return (
-    <article class="flex flex-col items-center gap-7">
+    <article class={styles.pokemons__page}>
       <section class="flex flex-col items-center">
         <span class="text-5xl">Status</span>
         <span>Página actual: {pokemonState.currentPage}</span>
@@ -23,29 +38,20 @@ export default component$(() => {
       <div>
         <button
           class="btn btn-primary mr-2"
-          onClick$={() => pokemonState.currentPage--}
-        >
-          Anteriores
-        </button>
-        <button
-          class="btn btn-primary mr-2"
           onClick$={() => pokemonState.currentPage++}
         >
           Siguientes
         </button>
       </div>
-      <section class="grid grid-cols-6 gap-3">
-        {/* {pokemons.value.map((pokemon) => (
-          <div
-            key={pokemon.name}
-            class="flex flex-col justify-center items-center p-2 bg-slate-100 rounded-lg  shadow-md shadow-indigo-500/50"
-          >
+      <section class={styles.pokemons__grid}>
+        {pokemonState.pokemons.map((pokemon) => (
+          <div key={pokemon.name} class={styles.pokemons__card}>
             <PokemonImage id={pokemon.id} isVisibleInit={true} />
             <span class="capitalize font-semibold text-indigo-500">
               {pokemon.name}
             </span>
           </div>
-        ))} */}
+        ))}
       </section>
     </article>
   )
